06b0d6cce0aee9042975d1ad6979fdff93682074
Medina <anyonegirl6@gmail.com>
2021-03-14 13:24:43 +0200

    css update

--- src/components/ShowRecipe.js
+++ src/components/ShowRecipe.js
@@ -7,16 +7,14 @@ function ShowRecipe({ recipe, setShowRecipe }) {
   }
   return (
     <div className="show-recipe">
-    <img className="pin" src="http://www.pngall.com/wp-content/uploads/4/Red-Pin-Transparent.png" alt=""/>
       <h1 className="show-recipe-header">{recipe.label}
-        <img className="show-separator" src="https://www.pidl.org/site-assets/images/line.png/@@images/image.png" alt="" />
       </h1>
       <div className="show-main">
         <div className="show-left">
           <img className="show-recipe-image" src={recipe.image} alt="" />
           <p className="show-recipe-calories">  Calories: {(recipe.calories).toFixed(2)}</p>
           <p className="show-recipe-wigth">  Total weight: {(recipe.totalWeight).toFixed(2)}</p>
-        
+          <button className="back-to-rec" onClick={setDisplayToNone}>Cancel</button>
         </div>
         <div className="show-right">
           <ol className="show-recipe-ingredients-ol">
@@ -28,7 +26,7 @@ function ShowRecipe({ recipe, setShowRecipe }) {
         </div>
         
       </div>
-      <button className="back-to-rec" onClick={setDisplayToNone}>Cancel</button>
+    
     </div>
   )
 }
