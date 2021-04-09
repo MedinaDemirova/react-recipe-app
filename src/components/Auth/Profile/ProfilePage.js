268bb5199cac42103f8e90c55475133640bf0b6d
Medina <anyonegirl6@gmail.com>
2021-04-09 22:51:33 +0300

    profile page works

--- src/components/Auth/Profile/ProfilePage.js
+++ src/components/Auth/Profile/ProfilePage.js
@@ -10,6 +10,7 @@ function ProfilePage() {
             <div className="user-info-image">
                 <div className="user-info">"Cooking is like love. It should be entered into with abandon or not at all."</div>
                 <div className="user-info-sub">Harriet Van Horne</div>
+                <div className="profile-picture" > {user.email? user.email.split("@")[0].toUpperCase(): null}</div>
             </div>
             <div className="user-favourites-list">favorites</div>
 
