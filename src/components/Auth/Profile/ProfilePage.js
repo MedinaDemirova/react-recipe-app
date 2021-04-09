4976d19f1f4c74480d8a40362fddf52199271d99
Medina <anyonegirl6@gmail.com>
2021-04-09 20:03:29 +0300

    profile page  works

--- src/components/Auth/Profile/ProfilePage.js
+++ src/components/Auth/Profile/ProfilePage.js
@@ -1,15 +1,18 @@
 import UserContext from "../../contexts/UserContext";
-import {useContext} from 'react';
+import { useContext } from 'react';
 import ".//ProfilePage.css"
 
-function ProfilePage(){
-let [user,setUser] = useContext(UserContext)
-    return(
-        <div className= "profile-page">
-        <div className ="user-info">Welcome to your favourite recipes list, {user.email? user.email.split('@')[0].toUpperCase():null}!</div>
+function ProfilePage() {
+    let [user, setUser] = useContext(UserContext)
+    return (
+        <div className="profile-page">
 
-        <div className="user-favourites-list">favorites</div>
-        
+            <div className="user-info-image">
+                <div className="user-info">"Cooking is like love. It should be entered into with abandon or not at all."</div>
+                <div className="user-info-sub">Harriet Van Horne</div>
+            </div>
+            <div className="user-favourites-list">favorites</div>
+
         </div>
     )
 }
