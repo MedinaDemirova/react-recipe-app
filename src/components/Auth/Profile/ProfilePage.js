98bd9b0b081d574729a07e0145844cb556617941
Medina <anyonegirl6@gmail.com>
2021-04-12 09:46:37 +0300

    profile page updates

--- src/components/Auth/Profile/ProfilePage.js
+++ src/components/Auth/Profile/ProfilePage.js
@@ -1,25 +1,35 @@
 import UserContext from "../../contexts/UserContext";
 import { useContext } from 'react';
 import ".//ProfilePage.css";
+import { Link, Route } from "react-router-dom";
 
 
 function ProfilePage() {
     let [user, setUser] = useContext(UserContext);
 
-  
+    function getFavRecipes() {
+
+    }
     return (
         <div className="profile-page">
 
             <div className="user-info-image">
                 <div className="user-info">"Cooking is like love. It should be entered into with abandon or not at all."</div>
                 <div className="user-info-sub">Harriet Van Horne</div>
-                <div className="profile-picture" > {user.email? user.email.split("@")[0].toUpperCase(): null}</div>
+               <Link to="/auth/my-profile"> <div className="profile-picture" > {user.email ? user.email.split("@")[0].toUpperCase() : null}</div></Link>
             </div>
-            <div className="user-favourites-list">
-             <div className="favourites-box">My favourites</div>
-             <div className="favourites-box">Find your new best recipe</div>
-             <div className="favourites-box">Try out todays's most populars</div>
-            </div>
+
+            <Route path="/auth/my-profile/favourites">
+            <div className="user-favourites-list">Fav</div>
+            </Route>
+
+            <Route path="/auth/my-profile" exact>
+                <div className="user-favourites-list">
+                    <Link to="/auth/my-profile/favourites"><div onClick={getFavRecipes} className="favourites-box first">My favourites</div></Link>
+                    <Link to="/"> <div className="favourites-box second">Find your new best recipe</div></Link>
+                    <Link to="/drinks"> <div className="favourites-box third">Try out new cocktail tonight</div></Link>
+                </div>
+            </Route>
 
         </div>
     )
