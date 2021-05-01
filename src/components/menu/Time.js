02de5f92cd8012464fbfeb3c3c1c4cc37a8708c8
Medina <anyonegirl6@gmail.com>
2021-05-01 14:26:31 +0300

    time format changed

--- src/components/menu/Time.js
+++ src/components/menu/Time.js
@@ -2,13 +2,15 @@ import { useEffect, useState } from "react";
 
 function Time() {
 
-    let time = new Date().toLocaleTimeString();
+    let time = new Date().toLocaleTimeString('en-GB');
+ 
     let [currentTime, setCurrentTime] = useState(time);
 
     useEffect(() => { setInterval(() => updateTimeState(), 1000) }, []);
 
     function updateTimeState() {
-        setCurrentTime(new Date().toLocaleTimeString());
+      
+        setCurrentTime(new Date().toLocaleTimeString('en-GB'));
     }
 
     return (
